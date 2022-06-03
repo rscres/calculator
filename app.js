28cd3a4bdc43afac171d1a0b5cac8c0f77b50791
rscres <renatocrescenti@gmail.com>
2022-06-03 18:52:40 -0300

    added equal and clear button function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
+    console.log(a, b, operator);
     switch (operator) {
         case '+':
             return add(a, b)
@@ -17,18 +18,50 @@ function operate(operator, a, b) {
         case '/':
             return divide(a, b)
             break;
-        default:
-            operator;
     }
 };
 
 const mainDisplay = document.querySelector('#main-display');
+const secondaryDisplay = document.querySelector('#secondary-display');
+let displayValues = '';
+
+function equalBtn(e) {
+    if (displayValues == '') return;
+    let operators = displayValues.split(' ');
+    let valueA = parseFloat(operators[0]);
+    let valueB = parseFloat(operators[2]);
+    if (operators[0] === null || valueA === null || valueB === NaN) return;
+    secondaryDisplay.textContent = displayValues;
+    displayValues = operate(operators[1], valueA, valueB);
+    mainDisplay.textContent = displayValues;
+};
+
+function clearBtn(e) {
+    if (e.target.dataset.value === 'AC') {
+        operators = [];
+        displayValues = '';
+        mainDisplay.textContent = displayValues;
+        secondaryDisplay.textContent  = displayValues;
+    } else if (e.target.dataset.value === 'C') {
+        displayValues = ''
+        mainDisplay.textContent = displayValues;
+    }
+}
 
 const buttons = document.querySelector('#buttons-container');
 buttons.addEventListener('click', function(e) {
-    if (e.target.tagName === 'BUTTON') {
-        const button = e.target;
-        console.log(button);
-        mainDisplay.textContent += button.dataset.value;
+    const button = e.target;
+    if (e.target.className === 'number btn') {
+        displayValues += button.dataset.value;
+        mainDisplay.textContent = displayValues;
     }
+    if (e.target.className === 'operand btn') {
+        if (displayValues == '') return;
+        displayValues += button.dataset.value;
+        mainDisplay.textContent = displayValues;
+    }
+    if(e.target.className === 'clear btn') return clearBtn(e);
+    if(e.target.className === 'equal btn') return equalBtn(e);
+    
 });
+
